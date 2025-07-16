6613bdedcd2f45a5bac4aa372a05087a51b2fbbe
Bittner89 <bittner-89@web.de>
2025-07-16 14:39:40 +0200

    requirements.txt erstellt, list_utils.py erstellt.

---
+++ list_utils.py
@@ -0,0 +1,15 @@
+# list_utils.py
+def find_max(numbers):
+    """Findet die größte Zahl in einer Liste."""
+    # TODO 1: Implementiere die Logik, um die größte Zahl zu finden.
+    # Tipp: Du kannst die eingebaute Funktion max() verwenden.
+    return max(numbers)
+    
+
+def remove_duplicates(items):
+    """Entfernt Duplikate aus einer Liste und gibt eine neue Liste zurück."""
+    # TODO 2: Implementiere die Logik zum Entfernen von Duplikaten.
+    # Tipp: Ein Set kann verwendet werden, um Duplikate zu entfernen.
+    duplikate_enf = set(items)
+    new_list = list(duplikate_enf)
+    return new_list    
